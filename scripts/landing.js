dcde32d49b687933f2970a96fc7d0145faaf4eca
Nilay Patel <nilaypatel044@gmail.com>
2017-07-29 01:08:15 -0400

    module2-checkpoint10-interfaces: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point) {
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.msTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
+}
+
 var animatePoints = function(points) {
-  var revealPoint = function(index) {
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-  }
-  for (var i = 0; i < points.length; i++){
-    revealPoint();
-  }
+  forEach(points, revealPoint);
 };
 
 window.onload = function() {
@@ -17,10 +16,9 @@ window.onload = function() {
     animatePoints(pointsArray);
   }
 
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
   window.addEventListener('scroll', function(event) {
-  if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+  if(pointsArray[0].getBoundingClientRect().top <= 500){
     animatePoints(pointsArray);
   }
 });
