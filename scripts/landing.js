75e7e95c6a0b97229992c9afdc964d7f964f76c0
Nilay Patel <nilaypatel044@gmail.com>
2017-07-28 22:58:32 -0400

    revert the changes created by merging assignment-m2c9-interfaces

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,15 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var revealPoint = function(point) {
-  point.style.opacity = 1;
-  point.style.transform = "scaleX(1) translateY(0)";
-  point.style.msTransform = "scaleX(1) translateY(0)";
-  point.style.WebkitTransform = "scaleX(1) translateY(0)";
-}
-
 var animatePoints = function(points) {
-  forEach(points, revealPoint);
+  var revealPoint = function(index) {
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].style.msTransform = "scaleX(1) translateY(0)";
+    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+  }
+  for (var i = 0; i < points.length; i++){
+    revealPoint();
+  }
 };
 
 window.onload = function() {
@@ -16,9 +17,10 @@ window.onload = function() {
     animatePoints(pointsArray);
   }
 
-
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
   window.addEventListener('scroll', function(event) {
-  if(pointsArray[0].getBoundingClientRect().top <= 500){
+  if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
     animatePoints(pointsArray);
   }
 });
