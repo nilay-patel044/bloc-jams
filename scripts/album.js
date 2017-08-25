aa96a84f4701aaa82e4b9a70cbbbe1cb65f1fb41
Nilay Patel <nilaypatel044@gmail.com>
2017-08-24 22:19:08 -0400

    module2-checkpoint20-interfaces

--- scripts/album.js
+++ scripts/album.js
@@ -193,6 +193,25 @@ var updatePlayerBarSong = function() {
 }
 
 
+var togglePlayFromPlayerBar = function() {
+    var $currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+    if(currentSoundFile && currentSoundFile.isPaused()) {
+        //Update play/pause button in cell.
+        $currentlyPlayingCell.html(pauseButtonTemplate);
+        //Update play/pause button in player bar using $(this) - the clicked element.
+        $(this).html(playerBarPauseButton);
+        currentSoundFile.play();
+    }
+    else if(currentSoundFile) {
+        //Update play/pause button in cell.
+        $currentlyPlayingCell.html(playButtonTemplate);
+        //Update play/pause button in player bar using $(this) - the clicked element.
+        $(this).html(playerBarPlayButton);
+        currentSoundFile.pause();
+    }
+}
+
+
 // Album button templates
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
@@ -208,12 +227,13 @@ var currentVolume = 80;
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
 var albumImage = $('.album-cover-art');
-
+var $playPauseButton = $('.main-controls .play-pause');
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
+    $playPauseButton.click(togglePlayFromPlayerBar());
 
     //});
     var albums = [albumPicasso, albumMarconi, albumSubway];
